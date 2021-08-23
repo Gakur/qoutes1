b1669d774a4f5a9e64c3aa2b0d960e34c0c44c89
Peter Gakure <petergakure97@gmail.com>
2021-08-23 15:15:43 +0300

    Add modified file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuotesComponent } from './quotes/quotes.component';
 import { FormComponent } from './forms/forms.component';
 import { DetailsComponent } from './details/details.component';
 import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HighlightDirective } from './highlight.directive';
     QuotesComponent,
     FormComponent,
     DetailsComponent,
-    HighlightDirective
+    HighlightDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
